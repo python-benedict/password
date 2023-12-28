d53c9fd0aa670e1b1247a3b440d0cf3b8c32e94f
python-benedict <newlyf38@gmail.com>
2023-12-28 12:06:04 +0000

    added a userRegistrationForm to the forms.py file

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -6,3 +6,20 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'firstname', 'last_name', 'email')
+        
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match')
+        else:
+            return cd['password2']
+        
+
+
