a9d170ba5b5fe374425668985473fb5c450ab08c
python-benedict <newlyf38@gmail.com>
2023-12-29 13:09:41 +0000

    okay

--- authentication/settings.py
+++ authentication/settings.py
@@ -138,6 +138,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'benedictosefaosei@gmail.com'
-EMAIL_HOST_PASSWORD = 'njyk imsp jcbj mawi'
+EMAIL_HOST_PASSWORD = ''
 
 BASE_URL = 'http://localhost:8000'
